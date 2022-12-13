2aeb4deb26ba5fd971752a05008b00cc6c3a91f5
vikas62081 <vikas62081@gmail.com>
2022-12-13 07:05:06 +0530

    pushing day 6

---
+++ javaScript/index.js
@@ -0,0 +1,12 @@
+// console.log("Hello");
+
+// let number; // variable declration
+// const age = 8; // variable initialization
+// const name = "John";
+// isAllowed = false;
+// const price = 45.6;
+// console.log(price);
+let firstName = "John";
+let lastName = "avs";
+
+console.log(firstName + lastName);
