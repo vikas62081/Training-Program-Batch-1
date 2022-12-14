ec013940f14a00a3ff2348e2ef2594c1f5756ab5
vikas62081 <vikas62081@gmail.com>
2022-12-14 07:05:07 +0530

    pushing data type and introduction from js

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,12 +1,63 @@
-// console.log("Hello");
+// let numberVariable = 45; //Type of variable a is number
+// console.log(numberVariable);
 
-// let number; // variable declration
-// const age = 8; // variable initialization
-// const name = "John";
-// isAllowed = false;
-// const price = 45.6;
-// console.log(price);
-let firstName = "John";
-let lastName = "avs";
+// numberVariable = "Hello"; //Type of variable a is string
 
-console.log(firstName + lastName);
+// console.log(numberVariable);
+// variable naming
+// const sumOfTwoNumbers = firstNumber + secondNumber;
+
+// let number = 6; //variable initialization
+// let firstName; //variable declaration
+
+//block scope
+// {
+//   //   let number = 4;
+//   console.log(number);
+//   number = number + 6;
+//   console.log(number);
+// }
+
+// console.log(number);
+
+// function scope
+// const firstNumber = 2; //golobal variable
+// const secondNumber = 7;
+// function sumOfTwoNumbers() {
+//   // function defination
+//   const firstNumber = 12; //local variable
+//   const secondNumber = 47;
+//   console.log("inside function ", firstNumber, secondNumber);
+// }
+// sumOfTwoNumbers(); // function call
+// console.log("outside function", firstNumber, secondNumber);
+
+// Data Type
+//  1. primitive data type
+let firstNumber = 7; //number
+let secondNumber = "7"; //string
+let name = "John"; // string
+let bool = true || false; //boolean
+let age; // undefined
+
+// 78653=>78,653
+// console.log(typeof firstNumber, typeof secondNumber);
+// console.log(firstNumber == secondNumber);
+// console.log(firstNumber === secondNumber);
+
+// == compare values alone
+// === compare values along with type
+
+//  2. non -primitive data type  (Object, array)
+//Object
+const pen = { height: 20, radius: 5, color: "white", isBallPen: true };
+console.log(pen);
+//Array
+const names = ["John", "Ben"];
+const numbers = [1, 2, 3, 4, 5];
+const pens = [
+  { height: 20, radius: 5, color: "white", isBallPen: true },
+  { height: 18, radius: 5, color: "BLACK", isBallPen: false },
+];
+console.log(pens);
+//Object
