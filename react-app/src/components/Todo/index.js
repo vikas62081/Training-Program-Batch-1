677b03cc83c5a90212428a5655841eb77be65f48
vikas62081 <vikas62081@gmail.com>
2023-01-09 07:51:04 +0530

    adding complete todo

--- react-app/src/components/Todo/index.js
+++ react-app/src/components/Todo/index.js
@@ -1,6 +1,25 @@
-import { PlusOutlined } from "@ant-design/icons";
-import { Button, Col, Input, List, Row, Typography } from "antd";
+import {
+  DeleteOutlined,
+  PlusOutlined,
+  SlackSquareOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  Col,
+  Input,
+  List,
+  message,
+  Popconfirm,
+  Row,
+  Tag,
+  Tooltip,
+  Typography,
+} from "antd";
 import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 const initialTodos = [
   {
     id: 1,
@@ -18,9 +37,9 @@ const initialTodos = [
     status: "pending",
   },
   {
-    id: 3,
+    id: 4,
     title: "Buy a new laptop",
-    status: "pending",
+    status: "completed",
   },
 ];
 const Todo = () => {
@@ -29,10 +48,30 @@ const Todo = () => {
   const handleChange = (e) => {
     setNewTodoTitle(e.target.value);
   };
+
   const addNewTodo = () => {
-    const newTodo = { id: 4, title: newTodoTitle, status: "pending" };
+    const newTodo = { id: uuid(), title: newTodoTitle, status: "pending" };
     setTodos([...todos, newTodo]);
     setNewTodoTitle("");
+    message.success("New todo added successfully");
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addNewTodo();
+  };
+  const handleCheckboxChange = (id) => {
+    const updatedTodos = todos.map((t) => {
+      if (t.id === id) {
+        return { ...t, status: "completed" };
+      }
+      return t;
+    });
+    setTodos(updatedTodos);
+  };
+  const handleDelete = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+    message.success("Todo get deleted successfully");
   };
   return (
     <>
@@ -43,23 +82,90 @@ const Todo = () => {
       ))} */}
       <Row justify="center">
         <Col span={10}>
-          <Input.Group compact>
-            <Input
-              name="newTodoTitle"
-              value={newTodoTitle}
-              onChange={handleChange}
-              placeholder="Add your new todo..."
-              size="large"
-              style={{ width: "calc(100% - 50px)" }}
-            />
-            <Button type="primary" size="large" onClick={addNewTodo}>
-              <PlusOutlined />
-            </Button>
-          </Input.Group>
+          <form onSubmit={handleFormSubmit} autoComplete="off">
+            <Input.Group compact>
+              <Input
+                name="newTodoTitle"
+                value={newTodoTitle}
+                onChange={handleChange}
+                placeholder="Add your new todo..."
+                size="large"
+                style={{ width: "calc(100% - 50px)" }}
+                required
+              />
+              <Button type="primary" size="large">
+                <PlusOutlined />
+              </Button>
+            </Input.Group>
+          </form>
           <List
             bordered
             dataSource={todos}
-            renderItem={(todo) => <List.Item>{todo.title}</List.Item>}
+            renderItem={(todo) => (
+              <>
+                <List.Item key={todo.title}>
+                  <List.Item.Meta
+                    avatar={
+                      <Avatar icon={<UserOutlined />} />
+                      // <Avatar>{todo.title.toUpperCase().slice(0, 2)}</Avatar>
+                    }
+                    title={
+                      <Checkbox
+                        onChange={() => handleCheckboxChange(todo.id)}
+                        disabled={todo.status === "completed"}
+                        checked={todo.status === "completed"}
+                      >
+                        {todo.title}
+                      </Checkbox>
+                    }
+                    description={
+                      <Tag
+                        color={
+                          todo.status === "completed" ? "success" : "warning"
+                        }
+                      >
+                        {todo.status}
+                      </Tag>
+                    }
+                  />
+                  <div>
+                    {" "}
+                    <Popconfirm
+                      title="Delete todo"
+                      description="Are you sure to delete this todo?"
+                      onConfirm={() => handleDelete(todo.id)}
+                      okText="Yes"
+                      cancelText="No"
+                    >
+                      <Tooltip title="Delete">
+                        <DeleteOutlined />
+                      </Tooltip>
+                    </Popconfirm>
+                  </div>
+                </List.Item>
+                {/* <List.Item>
+                  <Checkbox
+                    onChange={() => handleCheckboxChange(todo.id)}
+                    disabled={todo.status === "completed"}
+                    checked={todo.status === "completed"}
+                  >
+                    {todo.title}
+                  </Checkbox>
+                  <Popconfirm
+                    title="Delete todo"
+                    description="Are you sure to delete this todo?"
+                    onConfirm={() => handleDelete(todo.id)}
+                    // onCancel={cancel}
+                    okText="Yes"
+                    cancelText="No"
+                  >
+                    <Tooltip title="Delete">
+                      <DeleteOutlined />
+                    </Tooltip>
+                  </Popconfirm>
+                </List.Item> */}
+              </>
+            )}
           />
         </Col>
       </Row>
